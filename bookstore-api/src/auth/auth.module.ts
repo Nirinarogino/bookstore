61f640bf41c8f31b13f7b6a80a119212124dd8c9
THE-AUDIO <nandraina.dev22@gamail.com>
2024-01-09 19:08:30 +0300

    bookstore-api(fix-error-add-book)

--- bookstore-api/src/auth/auth.module.ts
+++ bookstore-api/src/auth/auth.module.ts
@@ -4,13 +4,11 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities';
 import { JwtModule } from '@nestjs/jwt';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategy/jwt.strategy';
 
-dotenv.config()
-
-
+dotenv.config();
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
@@ -18,15 +16,14 @@ dotenv.config()
       defaultStrategy: 'jwt',
     }),
     JwtModule.register({
-      global: true,
       secret: process.env.JWT_KEY,
       signOptions: {
-         expiresIn: '3600',
-         algorithm: 'HS512',
-         },
+        expiresIn: 3600, // expire en 1 heure (3600 secondes)
+        algorithm: 'HS512',
+      },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService,JwtStrategy],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
