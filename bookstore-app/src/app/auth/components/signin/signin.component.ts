2c11716e3906552f11874b71efa60c02caea8005
THE-AUDO <nandraina.dev22@gmail.com>
2024-01-12 18:22:06 +0300

    feat(bookstore-app): add a signin page

--- bookstore-app/src/app/auth/components/signin/signin.component.ts
+++ bookstore-app/src/app/auth/components/signin/signin.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class SigninComponent {
 
+  // user!: SocialUser;
+  // loggedIn!: boolean;
+
+  // constructor(
+  //   private socialAuthService: SocialAuthService,
+  //   private router: Router
+  // ) { }
+
+  // ngOnInit() {
+    
+  // }
+
+  // onSiginGoogle(): void {
+  //   this.socialAuthService.authState.pipe(
+  //     tap((response) => {
+  //       this.user = response;
+  //       this.loggedIn = response != null;
+  //       console.log(response);
+  //     }),
+  //     take(1)
+  //   ).subscribe();
+  // }
+
 }
