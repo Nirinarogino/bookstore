28f05b77a0b19509e1cbdc89203718b1fc22fd47
THE-AUDO <nandraina.dev22@gmail.com>
2024-01-15 23:03:17 +0300

    initiate a home page

--- bookstore-app/src/app/featur/featur.module.ts
+++ bookstore-app/src/app/featur/featur.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { FeaturRoutingModule } from './featur-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     CommonModule,
-    FeaturRoutingModule
+    FeaturRoutingModule,
+    SharedModule
   ]
 })
 export class FeaturModule { }
