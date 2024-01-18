7edec1c70ecab36777414e23310a5d5ecb67f23a
THE-AUDO <nandraina.dev22@gmail.com>
2024-01-18 23:40:40 +0300

    error in 50% resolve

--- bookstore-api/src/book/book.controller.ts
+++ bookstore-api/src/book/book.controller.ts
@@ -1,16 +1,34 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards, UseInterceptors } from '@nestjs/common';
 import { addBookDto } from './dto/addBook.dto';
 import { BookService } from './book.service';
 import { UserReq } from 'src/decorators/user.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import multer, { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
 
 @Controller('book')
 export class BookController {
     constructor (
         private bookService: BookService
         ) {}
+// ===================== declaration of the const =====================
+
+storage = multer.diskStorage({
+    destination: './book_images',
+    filename: function (req, file, cb) {
+        const filename: string = path.parse(file.originalname).name.replace(/\\/g, '') + uuidv4();
+        const extension: string = path.parse(file.originalname).ext;
+        cb(null, filename + extension)
+    }
+})
+ upload = multer({ storage: this.storage })
+
+
 // =================== add book to the bookstore ===============
     @Post('add')
+    @UseInterceptors(FileInterceptor('file'))
     @UseGuards(JwtAuthGuard)
     async addBook(
         @Body() book: addBookDto,
