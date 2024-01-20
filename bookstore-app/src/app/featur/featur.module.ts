5d251c19b2f431ea50f14590736fe812a3e03ee2
THE-AUDO <nandraina.dev22@gmail.com>
2024-01-20 23:02:06 +0300

    en fin je comprend but i should fix some error

--- bookstore-app/src/app/featur/featur.module.ts
+++ bookstore-app/src/app/featur/featur.module.ts
@@ -5,17 +5,21 @@ import { FeaturRoutingModule } from './featur-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { SingleBookComponent } from './components/single-book/single-book.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    SingleBookComponent
+    SingleBookComponent,
+    AddBookComponent
   ],
   imports: [
     CommonModule,
     FeaturRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class FeaturModule { }
