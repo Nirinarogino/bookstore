9c129d9e49b0464f6e553f04ed6ee593afb0ba7f
THE-AUDO <nandraina.dev22@gmail.com>
2024-02-22 13:00:25 +0300

    add typing animation

--- bookstore-app/src/app/shared/shared.module.ts
+++ bookstore-app/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { MaterialModule } from './material.module';
 import { ShortPipe } from './pipes/short_text.pipe';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 @NgModule({
   declarations: [
@@ -11,10 +12,12 @@ import { ShortPipe } from './pipes/short_text.pipe';
   imports: [
     CommonModule,
     SharedRoutingModule,
+    NgxTypedJsModule
   ],
 exports: [
   MaterialModule,
-  ShortPipe
+  ShortPipe,
+  NgxTypedJsModule
 
 ],
 })
