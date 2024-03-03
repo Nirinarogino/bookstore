dc3cd40f8eeaa6b896ec98c62b5bf00b20cb00e0
THE-AUDO <nandraina.dev22@gmail.com>
2024-03-03 23:05:50 +0300

    saika vita

--- bookstore-api/src/book-borrowed/book-borrowed.service.ts
+++ bookstore-api/src/book-borrowed/book-borrowed.service.ts
@@ -17,10 +17,13 @@ export class BookBorrowedService {
         private borrowedBookRepository: Repository<BorrowedBook>
     ){}
 
-    async borrowedBook(user_id: number, book_id: number, bookDate: borrowedDateDto ) {
-        const userWhoBorrowed = await this.userRepository.findOne({ where: { userId: user_id } });
+    async borrowedBook(user: User, book_id: number, bookDate: borrowedDateDto ) {
+
+        const userWhoBorrowed = await this.userRepository.findOne( {where:{userName: user.userName}});
+        console.log(userWhoBorrowed);
         const bookWhichBorrowed = await this.bookRepository.findOne({ where:{ bookId: book_id } });
-        const borrowedBook = this.borrowedBookRepository.create(bookDate);
+        console.log(bookWhichBorrowed);
+        const borrowedBook = this.borrowedBookRepository.create();
         borrowedBook.book = bookWhichBorrowed;
         borrowedBook.user = userWhoBorrowed;
         borrowedBook.borrowedDate = bookDate.borrowedDate;
