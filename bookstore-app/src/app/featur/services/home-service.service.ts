46a842fd66045a6d6b53f78b61bb7743bcc983cd
THE-AUDO <nandraina.dev22@gmail.com>
2024-03-07 09:02:56 +0300

    get book by category from front

--- bookstore-app/src/app/featur/services/home-service.service.ts
+++ bookstore-app/src/app/featur/services/home-service.service.ts
@@ -10,10 +10,20 @@ export class HomeService {
         private http: HttpClient, // Correct typo here
         private router: Router // Correct typo here
     ) {}
-
+    token = sessionStorage.getItem('token');
     getAllBooks(): Observable<Book[]> {
         return this.http.get<Book[]>(`http://localhost:3000/book/all`); // Corrected http to this.http
     }
 
+    async getBookByCategory(category: string){
+        const bookCat = {
+            Category: category
+        }
+        return this.http.post(`http://localhost:3000/book-borrowed/${category}`, bookCat,{
+            headers: {
+              "Authorization": `Bearer ${this.token}`
+            }
+          })
+    }
   
 }
