cda25dc2e40e1d1d9096c4fbf195745b65781f62
THE-AUDO <nandraina.dev22@gmail.com>
2024-03-12 22:57:42 +0300

    animation

--- bookstore-app/src/app/featur/components/admin-page/admin-page.component.ts
+++ bookstore-app/src/app/featur/components/admin-page/admin-page.component.ts
@@ -1,31 +1,55 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
   styleUrls: ['./admin-page.component.scss']
 })
-export class AdminPageComponent implements OnInit{
+export class AdminPageComponent implements OnInit, AfterViewInit{
+  
   AlluserAndBook!: any;
   token = sessionStorage.getItem('token');     
    constructor(
-      private http: HttpClient
+      private http: HttpClient,
+      private renderer: Renderer2
     ){}
+  @ViewChild('haut') haut!: ElementRef
+  @ViewChild('bas') bas!: ElementRef
+  @ViewChild('book') book!: ElementRef
+  @ViewChild('user') user!: ElementRef
+  switchDiv(elt: any, deleteClass: any, addClass: any){
+      elt.nativeElement.addEventListener('click',()=>{
+      const classNow = deleteClass.nativeElement.classList.value
+      const classTest  = classNow.toLowerCase().includes('visible')
+      if(classTest){
 
- 
+          this.renderer.removeClass(deleteClass.nativeElement, 'visible')
+          this.renderer.addClass(deleteClass.nativeElement, 'hidden')
+          this.renderer.removeClass(addClass.nativeElement, 'hidden')
+          this.renderer.addClass(addClass.nativeElement, 'visible')
+      }
+      else{
+        console.log(classTest);
+        
+      }
+    })
+  }
+  ngAfterViewInit(): void {
+    this.switchDiv(this.user, this.haut, this.bas)
+    this.switchDiv(this.book, this.bas, this.haut)
+  } 
   getAllUserAndBokk(){
      this.AlluserAndBook =  this.http.get('http://localhost:3000/admin',{
       headers: {
         "Authorization": `Bearer ${this.token}`
       }
      })
-     console.log(this.AlluserAndBook.subscribe());
-
      return this.AlluserAndBook;
       
    }
    ngOnInit(): void {
     this.getAllUserAndBokk()
+    
   }
 }
