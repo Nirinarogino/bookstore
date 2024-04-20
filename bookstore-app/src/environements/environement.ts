b1236cca3b312e7feeb208f09e1293e4c8b2fe43
Nirinarogino <115354044+Nirinarogino@users.noreply.github.com>
2024-04-20 13:32:49 +0200

    Update environement.ts

--- bookstore-app/src/environements/environement.ts
+++ bookstore-app/src/environements/environement.ts
@@ -1,4 +1,4 @@
 export const environement = {
     client_id: '369981562513-82lmsgcorjn9itgkfdktgocgprm95btc.apps.googleusercontent.com',
-    apiUrl: 'http://localhost/book'
+    apiUrl: 'https://bookstore-8xgx.onrender.com'
 }
